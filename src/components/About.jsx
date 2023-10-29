37935732e1b2598ad6540dfccabb095868bdb430
Aygul Okdirova <aygul.okdirova@check24.de>
2023-10-29 13:34:26 +0100

    add SectionWrapper and Overview section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px'>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Asperiores
+        dolor dolorem recusandae aut nobis enim voluptatibus esse ea sed, illum
+        necessitatibus fuga officiis natus, ut, odit dolorum voluptas tempore
+        consequatur. Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+        Impedit voluptates esse architecto odio ipsum facere repellat, ut
+        debitis aperiam quod modi, id quidem hic dolorum officiis tenetur
+        assumenda enim. Dolor?
+      </motion.p>
+    </>
+  );
+};
 
-export default About
+export default SectionWrapper(About, "about");
