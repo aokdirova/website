752654241fc2e954de9b54ea5320a263d614f658
Aygul Okdirova <aygul.okdirova@check24.de>
2023-10-29 15:35:33 +0100

    BallCanvas and Tech section added

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,5 +1,63 @@
-const Ball = () => {
-  return <div>Ball</div>;
+import { Suspense, useState, useEffect } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  Decal,
+  Float,
+  OrbitControls,
+  Preload,
+  useTexture,
+} from "@react-three/drei";
+import CanvasLoader from "../Loader";
+
+const Ball = ({ imgUrl }) => {
+  const [decal] = useTexture([imgUrl]);
+  return (
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        <icosahedronGeometry args={[1, 2]} />
+        <meshStandardMaterial
+          color='#fff'
+          polygonOffset
+          polygonOffsetFactor={-4}
+          flatShading
+        />
+        <Decal
+          map={decal}
+          position={[0, 0, 1]}
+          rotation={[2 * Math.PI, 0, 6.25]}
+          flatShading
+        />
+      </mesh>
+    </Float>
+  );
 };
 
-export default Ball;
+const BallCanvas = ({ icon }) => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  //TODO move this to the utils and make a custom hook of it to check if isMobile and isTablet
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+  return (
+    <Canvas frameloop='demand' gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default BallCanvas;
