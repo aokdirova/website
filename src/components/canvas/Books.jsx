e6357713f698b16f32dbc8bdbd609227cc6c20ff
Aygul Okdirova <aygul.okdirova@check24.de>
2023-10-30 21:23:59 +0100

    add smaller glb file for books

--- src/components/canvas/Books.jsx
+++ src/components/canvas/Books.jsx
@@ -4,7 +4,7 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Books = ({ isMobile }) => {
-  const books = useGLTF("./books/scene.glb");
+  const books = useGLTF("./books/wizardry_still_life.glb");
   return (
     <mesh>
       <hemisphereLight intensity={0.9} groundColor='black' />
