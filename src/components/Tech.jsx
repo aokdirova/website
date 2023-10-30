3e53015b092147718631ee3001afe9623d32257d
Aygul Okdirova <aygul.okdirova@check24.de>
2023-10-30 09:43:22 +0100

    projects section added and small adjustments to tech section

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -3,15 +3,18 @@ import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map(({ name, icon }) => {
-        return (
-          <div className='w-28 h-28 cursor-pointer' key={name}>
-            <BallCanvas icon={icon} />
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <h1 className='pb-8'>Click balls to move them around</h1>
+      <div className='flex flex-row flex-wrap justify-center gap-10'>
+        {technologies.map(({ name, icon }) => {
+          return (
+            <div className='w-28 h-28 cursor-pointer' key={name}>
+              <BallCanvas icon={icon} />
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
