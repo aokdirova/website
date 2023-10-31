5ee62e927a2e089b3a0d5c5a1a8f462c7dc39bc8
Aygul Okdirova <aygul.okdirova@check24.de>
2023-10-31 11:12:01 +0100

    reduce size of image

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat'>
+        <div className='bg-primary bg-hero-pattern bg-cover bg-no-repeat'>
           <div className='bg-[#151414d4]'>
             <Navbar />
             <Hero />
