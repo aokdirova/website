2c0952c6ec28c87c88b86b5c1270965e97db26b4
Aygul Okdirova <aygul.okdirova@check24.de>
2023-11-01 22:57:17 +0100

    update stars canvas for hero

--- src/components/canvas/StarsForHero.jsx
+++ src/components/canvas/StarsForHero.jsx
@@ -1,8 +1,28 @@
-import { Suspense } from "react";
-import { Canvas } from "@react-three/fiber";
-import { Preload } from "@react-three/drei";
-import { Stars } from "./Stars";
+import { Suspense, useRef } from "react";
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Points, PointMaterial, Preload } from "@react-three/drei";
+import * as random from "maath/random/dist/maath-random.esm";
 
+export const Stars = (props) => {
+  const ref = useRef();
+  const sphere = random.inSphere(new Float32Array(5000), { radius: 1 });
+
+  useFrame((state, delta) => {
+    ref.current.rotation.x -= delta / 100;
+    ref.current.rotation.y -= delta / 100;
+  });
+  return (
+    <group rotation={[0, 0, Math.PI / 6]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          color='#f272c8'
+          size={0.001}
+          sizeAttenuation={true}
+          depthWrite={false}></PointMaterial>
+      </Points>
+    </group>
+  );
+};
 const StarsForHero = () => {
   return (
     <div className='w-full h-auto absolute inset-0'>
