dae9e6674bc1d63dcd5165450654b59b4c55d834
Aygul Okdirova <aygul.okdirova@check24.de>
2023-11-13 12:36:01 +0100

    do not show stars canvas in hero if mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,23 @@
+import { useState, useEffect } from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { mainIconsdark } from "../assets";
 import { StarsForHero } from "./canvas";
 const Hero = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  //TODO move this to the utils and make a custom hook of it to check if isMobile and isTablet
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+    setIsMobile(mediaQuery.matches);
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
   return (
     <section className='relative w-full h-screen mx-auto'>
       <div
@@ -29,7 +44,8 @@ const Hero = () => {
           className='pt-20 h-full w-auto'
         />
       </div>
-      <StarsForHero />
+
+      {!isMobile && <StarsForHero />}
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-2xl border-4 flex justify-center items-start p-2'>
